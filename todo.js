28a7045c2f8ebed322250e9631dfdd9126080107
urijuk <gelte@mail.ru>
2021-01-09 00:22:49 +0600

    lesson19

---
+++ todo.js
@@ -0,0 +1,16 @@
+const todoItem=document.getElementsByName("todo_item")
+todoItem.addEventListener('click', function(){
+    alert("welcome")
+})
+
+
+
+
+const addcart = document.getElementById("create_button")
+addcart.addEventListener('click', function(){
+    alert("welcome")
+    // let todoItem=document.getElementsByClassName("todo_item")
+    // document.getElementsByClassName("todo_container").appendChild(todoItem);
+})
+
+
